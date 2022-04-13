81a615b3d54bd2eff1daedfce7c346624819ef0f
li.he <li.he@thoughtworks.com>
2022-04-13 16:31:30 +0800

    wechat

---
+++ WeChat_API.ts
@@ -0,0 +1,2 @@
+//https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+export const MINI_PROGRAM_TOKEN= "https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code"
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {MINI_PROGRAM_TOKEN} from "./WeChat_API";
+
 var express = require('express');
 var Webtask = require('webtask-tools');
 var bodyParser = require('body-parser');
@@ -14,22 +16,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/.well-known/openid-configuration', (req, res) => {
-    const wtUrl = `https://${req.headers.host}/auth0-singpass-extension`;
+    const wtUrl = `https://${req.headers.host}/auth0-wechat-extension`;
     res.status(200).send({
         "authorization_endpoint": `${wtUrl}/authorize`,
         "token_endpoint": `${wtUrl}/token`
     });
 });
 app.get('/authorize', (req, res) => {
-    const context = req.webtaskContext;
-    if (!req.query.client_id) {
-        return res.send(400, 'missing client_id');
-    }
-    if (context.data.AUTH0_CLIENT_ID !== req.query.client_id) {
-        return res.send(401, 'invalid client_id');
-    }
-    var url = `https://${context.data.AUTH0_CUSTOM_DOMAIN}${req.url}&ndi_state=${req.query.state}&ndi_nonce=${req.query.code_challenge}&singpass=true`;
-    res.redirect(url);
+
 });
 
 app.post('/token', async function (req, res) {
@@ -39,37 +33,13 @@ app.post('/token', async function (req, res) {
         return res.send(400, 'missing client_id / client_secret');
     }
     if (context.data.AUTH0_CLIENT_ID === client_id && context.data.AUTH0_CLIENT_SECRET === client_secret) {
-        const client_assertion = await generatePrivateKeyJWT(context.data);
         var options = {
-            method: 'POST',
-            url: `${context.data.SINGPASS_ENVIRONMENT}/token`,
-            headers: { 'content-type': 'application/x-www-form-urlencoded' },
-            data: qs.stringify({
-                grant_type: 'authorization_code',
-                client_id: context.data.SINGPASS_CLIENT_ID,
-                client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-                client_assertion: client_assertion,
-                code: code,
-                redirect_uri: redirect_uri
-            })
+            method: 'GET',
+            url: MINI_PROGRAM_TOKEN+`&appid=${context.data.WeChat_CLIENT_ID}&secret=${context.data.WeChat_CLIENT_SECRET}&js_code=${code}`,
         };
         try {
             const response = await axios.request(options);
-            const { id_token } = response.data;
-            const publicKey = await loadPublicKey(context.data);
-            const code_v = new TextEncoder().encode(code_verifier);
-            const code_v_s256 = crypto.createHash('sha256').update(code_v).digest('base64').replace(/\//g, '_').replace(/\+/g, '-').replace(/=/g, '');
-            console.log(`nonce expected: ${code_v_s256}`);
-            const { payload, protectedHeader } = await jwtVerify(id_token, publicKey, {
-                issuer: context.data.ISSUER,
-                audience: context.data.CLIENT_ID,
-            });
-            if (payload.nonce !== code_v_s256) {
-                return res.send(400, 'nonce mismatch');
-            } else {
-                response.data.payload = payload;
-                return res.status(200).send(response.data);
-            }
+            return res.status(200).send(response.data);
 
         } catch (error) {
             if (error.response) {
@@ -107,41 +77,6 @@ app.post('/verify', async function (req, res) {
         }
     }
 })
-async function loadPrivateKey(config) {
-    try {
-        const response = await axios.get(config.RELYING_PARTY_JWKS_ENDPOINT);
-        const { keys } = response.data;
-        keys[0].d = config.RELYING_PARTY_PRIVATE_KEY;
-        return await parseJwk(keys[0], config.SINGPASS_SIGNING_ALG);;
-    } catch (e) {
-        return e;
-    }
-};
-
-async function loadPublicKey(config) {
-    try {
-        const response = await axios.get(`${config.SINGPASS_ENVIRONMENT}/.well-known/keys`);
-        const publicKey = await parseJwk(response.data.keys[0], config.SINGPASS_SIGNING_ALG);
-        return publicKey;
-    } catch (e) {
-        return e;
-    }
-};
-
-async function generatePrivateKeyJWT(config) {
-    //const privateKeyPEM = crypto.createPrivateKey(config.PRIVATE_KEY.replace(/\\n/gm, '\n'));
-    const key = await loadPrivateKey(config);
-    const jwt = await new SignJWT({})
-        .setProtectedHeader({ alg: config.SINGPASS_SIGNING_ALG, kid: config.RELYING_PARTY_KID, typ: "JWT" })
-        .setIssuedAt()
-        .setIssuer(config.SINGPASS_CLIENT_ID)
-        .setSubject(config.SINGPASS_CLIENT_ID)
-        .setAudience(config.SINGPASS_ENVIRONMENT)
-        .setExpirationTime('2m') //The expiration time on or after which the JWT MUST NOT be accepted by NDI for processing. Additionally, NDI will not accept tokens with an exp longer than 2 minutes since iat. https://tools.ietf.org/html/rfc7519#section-4.1.4
-        .setJti(uuid.v4())
-        .sign(key);
-    return jwt;
-}
 
 
 module.exports = Webtask.fromExpress(app);
